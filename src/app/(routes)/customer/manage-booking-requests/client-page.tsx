2695ffe35bebfbf79721e164189556f20ccc0252
Patrick Bradley <pmbradley1997@gmail.com>
2025-09-26 12:51:06 -0400

    trying this again

--- src/app/(routes)/customer/manage-booking-requests/client-page.tsx
+++ src/app/(routes)/customer/manage-booking-requests/client-page.tsx
@@ -98,7 +98,7 @@ export default function ManageBookingRequestsClient({
   return (
     <div
       className="flex flex-col space-y-5"
-      style={{ height: "calc(100vh - 200px)" }} // I don't love this but it works
+      style={{ height: "calc(100vh - 250px)" }} // I don't love this but it works
     >
       <BookingRequestsStatsCards stats={stats} />
 
