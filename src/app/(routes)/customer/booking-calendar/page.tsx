3c437ec292de092dedf9af664718ef7a7f60ce4c
Patrick Bradley <pmbradley1997@gmail.com>
2025-09-26 13:12:18 -0400

    making the calendar even smaller

--- src/app/(routes)/customer/booking-calendar/page.tsx
+++ src/app/(routes)/customer/booking-calendar/page.tsx
@@ -13,7 +13,7 @@ export default async function CalendarPage() {
     <HydrateClient>
       <div
         className="mx-auto grid w-[90%] grid-rows-[auto_1fr] gap-3 pt-2 sm:w-3/4"
-        style={{ height: "calc(100vh - 100px)" }}
+        style={{ height: "calc(100vh - 120px)" }}
       >
         <PageHeader
           title={"Booking Calendar"}
