c6756c31642d1779de80243c6460c7d93813f330
Patrick Bradley <pmbradley1997@gmail.com>
2025-10-02 18:39:56 -0400

    Finishing up

--- src/app/(routes)/admin/scheduled-jumps/client-page.tsx
+++ src/app/(routes)/admin/scheduled-jumps/client-page.tsx
@@ -6,8 +6,6 @@ import { computeScheduledJumpStats } from "mydive/app/_shared-frontend/utils/sta
 import type { UserDto } from "mydive/server/api/routers/types";
 import { useMemo } from "react";
 import ScheduledJumpsTable from "mydive/app/_shared-frontend/components/tables/scheduled-jump/table";
-import { Card, CardBody } from "@nextui-org/react";
-import PageHeader from "mydive/app/_shared-frontend/components/headers/PageHeader";
 
 export default function AdminScheduledJumpsClient({
   scheduledJumps,
@@ -20,30 +18,17 @@ export default function AdminScheduledJumpsClient({
     return computeScheduledJumpStats(scheduledJumps);
   }, [scheduledJumps]);
   return (
-    <div className="z-0 p-4 md:p-8">
-      <div className="mx-auto max-w-7xl">
-        <PageHeader
-          title="Scheduled Jumps Manager"
-          description="See all of your scheduled jumps in one place"
+    <div
+      className="flex flex-col space-y-5"
+      style={{ height: "calc(100vh - 250px)" }} // I don't love this but it works
+    >
+      <ScheduledJumpsStatsCards stats={stats} />
+      <div className="flex h-full max-h-full flex-1 flex-col overflow-auto bg-white/95 p-0 shadow-2xl backdrop-blur-sm">
+        <ScheduledJumpsTable
+          scheduledJumps={scheduledJumps}
+          users={users}
+          isAdminView={true}
         />
-        {/* Main Content */}
-        <ScheduledJumpsStatsCards stats={stats} />
-
-        <div className="mb-[5px] min-h-0 flex-1">
-          <Card className="h-full bg-white/95 shadow-2xl backdrop-blur-sm">
-            <CardBody className="flex h-full flex-col p-0">
-              <div className="min-h-0 flex-1 overflow-hidden">
-                <div className="h-full overflow-auto">
-                  <ScheduledJumpsTable
-                    scheduledJumps={scheduledJumps}
-                    users={users}
-                    isAdminView={true}
-                  />
-                </div>
-              </div>
-            </CardBody>
-          </Card>
-        </div>
       </div>
     </div>
   );
--- src/app/(routes)/admin/scheduled-jumps/page.tsx
+++ src/app/(routes)/admin/scheduled-jumps/page.tsx
@@ -3,6 +3,7 @@ import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 import ScheduledJumpsClient from "./client-page";
+import PageHeader from "mydive/app/_shared-frontend/components/headers/PageHeader";
 
 // Probably need to implement some fetch that allows us to get all the admin users!
 
@@ -15,7 +16,17 @@ export default async function AdminScheduledJumpsManager() {
     await api.adminScheduledJumpsManager.getScheduledJumpsAndUsers();
   return (
     <HydrateClient>
-      <ScheduledJumpsClient scheduledJumps={scheduledJumps} users={users} />
+      <div
+        id="page-elements"
+        className="mx-auto w-[90%] space-y-5 pt-2 sm:w-3/4"
+        style={{ height: "calc(100vh - 150px)" }}
+      >
+        <PageHeader
+          title="Scheduled Jumps Manager"
+          description="See all of your scheduled jumps in one place"
+        />
+        <ScheduledJumpsClient scheduledJumps={scheduledJumps} users={users} />
+      </div>
     </HydrateClient>
   );
 }
